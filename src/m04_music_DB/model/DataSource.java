1c16d7a68a9ccc3a092fd58fadcb927e39a24d05
Krzysztof Jaczewski <krzysztof.jaczewski@op.pl>
2021-05-17 22:42:11 +0200

    m04_music_DB (1)

---
+++ src/m04_music_DB/model/DataSource.java
@@ -0,0 +1,49 @@
+package m04_music_DB.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DataSource {
+    public static final String DB_NAME = "music.db";
+    public static final String CONNECTION_STRING =
+            "jdbc:sqlite:/home/krzysztof/workspace/Buchalka/Databases/src/m04_music_DB/" + DB_NAME;
+
+    public static final String TABLE_SONGS = "songs";
+    public static final String COLUMN_SONGS_ID = "_id";
+    public static final String COLUMN_SONGS_TRACK = "track";
+    public static final String COLUMN_SONGS_TITLE = "title";
+    public static final String COLUMN_SONGS_ALBUM = "album";
+
+    public static final String TABLE_ALBUMS = "albums";
+    public static final String COLUMN_ALBUMS_ID = "_id";
+    public static final String COLUMN_ALBUMS_NAME = "name";
+    public static final String COLUMN_ALBUMS_ARTIST = "artist";
+
+    public static final String TABLE_ARTISTS = "artists";
+    public static final String COLUMN_ARTISTS_ID = "_id";
+    public static final String COLUMN_ARTISTS_NAME = "name";
+
+    private  Connection conn;
+
+    public boolean open() {
+        try {
+            conn = DriverManager.getConnection(CONNECTION_STRING);
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Couldn't open database: " + e.getMessage());
+            return false;
+        }
+    }
+
+    public void close() {
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            System.out.println("Couldn't close the connection: " + e.getMessage());
+        }
+    }
+}
