73a5bc14daeaa23149ef73ac8f7d170413e64d7d
Krzysztof Jaczewski <krzysztof.jaczewski@op.pl>
2021-05-18 22:56:42 +0200

    m04_music_DB (2) - pozyskiwanie informacji z tabel

--- src/m04_music_DB/Main.java
+++ src/m04_music_DB/Main.java
@@ -1,6 +1,9 @@
 package m04_music_DB;
 
+import m04_music_DB.model.Artist;
 import m04_music_DB.model.DataSource;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,6 +11,15 @@ public class Main {
         if (!dataSource.open()) {
             System.out.println("Can't open datasource");
             return;
+        }
+
+        List<Artist> artists = dataSource.queryArtist();
+        if (artists == null) {
+            System.out.println("No artists found!");
+            return;
+        }
+        for (Artist a : artists) {
+            System.out.println(a.getId() + ": " + a.getName());
         }
 
         dataSource.close();
